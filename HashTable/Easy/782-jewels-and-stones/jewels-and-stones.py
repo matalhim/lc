a6a1a6c3bab3c402d2933e55b66e805fce95c379
Matsvei Alkhimovich <94304426+matalhim@users.noreply.github.com>
2025-03-17 18:43:50 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.63%) - LeetSync

--- HashTable/Easy/782-jewels-and-stones/jewels-and-stones.py
+++ HashTable/Easy/782-jewels-and-stones/jewels-and-stones.py
@@ -1,9 +1,11 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewels_set: set[str] = set(jewels)
         freq: dict[str: int] = {}
+        summ: int = 0
         for char in stones:
-            if char in jewels_set:
                 freq[char] = freq.get(char, 0) + 1
-        
-        return sum(freq.values())
+
+        for char in jewels:
+            summ += freq.get(char, 0)
+ 
+        return summ
