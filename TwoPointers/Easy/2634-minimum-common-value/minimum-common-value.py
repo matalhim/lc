5a63dbc0d7be54033dc2f66898223f26a6eeee50
Matsvei Alkhimovich <94304426+matalhim@users.noreply.github.com>
2025-03-18 00:07:14 +0300

    Time: 4 ms (93.67%) | Memory: 36.9 MB (68.24%) - LeetSync

--- TwoPointers/Easy/2634-minimum-common-value/minimum-common-value.py
+++ TwoPointers/Easy/2634-minimum-common-value/minimum-common-value.py
@@ -1,17 +1,19 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1 = sorted(nums1)
-        nums2 = sorted(nums2)
+        if nums1[-1] < nums2[0] or nums2[-1] < nums1[0]:
+            return -1
+
         i: int = 0
         j: int = 0
-        while i < len(nums1) and j < len(nums2):
+        n1 , n2 = len(nums1),len(nums2)
+        while i < n1 and j < n2:
             if nums1[i] == nums2[j]:
                 return nums1[i]
-            elif nums1[i] < nums2[j]:
+            if nums1[i] < nums2[j] :
                 i += 1
             else:
                 j += 1
-        
+
         return -1
 
         
