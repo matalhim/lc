dd94da9dcb0ce5069dde148ac6d77930e3034ca1
Matsvei Alkhimovich <94304426+matalhim@users.noreply.github.com>
2025-03-20 21:26:23 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.23%) - LeetSync

---
+++ Sorting/Easy/2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        n_list: list = []
+        while num > 0:
+            n_list.append(num % 10)
+            num = num // 10
+        print(n_list)
+
+        return sum(sorted(n_list)[:2]) * 10 + sum(sorted(n_list)[2:])
+        
