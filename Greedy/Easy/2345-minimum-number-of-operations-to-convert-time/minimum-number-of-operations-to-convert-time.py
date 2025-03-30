603762f5cececfa68198ce8a1af008b30323283a
Matsvei Alkhimovich <94304426+matalhim@users.noreply.github.com>
2025-03-30 20:45:17 +0000

    Time: 0 ms (100.00%) | Memory: 17.7 MB (89.43%) - LeetSync

---
+++ Greedy/Easy/2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convertTime(self, current: str, correct: str) -> int:
+        current_h, current_m = map(int, current.split(':'))
+        correct_h, correct_m = map(int, correct.split(':'))
+        
+        current = current_h * 60 + current_m
+        correct = correct_h * 60 + correct_m
+        
+        if correct < current:
+            orrect += 24 * 60
+        
+        diff: int = correct - current
+        res: int = 0
+        
+        for t in [60, 15, 5, 1]:
+            if diff >= t:
+                res += diff // t
+                diff %= t
+        
+        return res
