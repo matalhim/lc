8bdef3a0917a8618d2585e01601b5e7759f3dc3b
Matsvei Alkhimovich <94304426+matalhim@users.noreply.github.com>
2025-04-19 22:50:58 +0300

    Time: 1715 ms (20.73%) | Memory: 18.4 MB (92.34%) - LeetSync

--- DynamicProgramming/Medium/45-jump-game-ii/jump-game-ii.py
+++ DynamicProgramming/Medium/45-jump-game-ii/jump-game-ii.py
@@ -1,11 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [len(nums) + 1] * len(nums)
+        n = len(nums)
+        dp = [n+1] * n
         dp[0] = 0
-        for i in range(len(nums)):
+        res = 0
+        for i in range(n):
+            if i > res:
+                break
             step = nums[i]
             for j in range(1, step + 1):
-                if i + j < len(nums):
-                    dp[i + j] = min(dp[i + j], dp[i] + 1)
-        return dp[len(nums) - 1]
+                if i + j < n:
+                    if dp[i + j] > dp[i] + 1:
+                        dp[i + j] = dp[i] + 1
+                        if i + j > res:
+                            res = i + j
+        return dp[n - 1]
 
